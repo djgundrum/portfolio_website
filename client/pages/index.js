cab35a6f082dddc9db77216b43f43829c5f06a13
FaintKomorebi <98120433+FaintKomorebi@users.noreply.github.com>
2022-01-20 15:38:53 -0500

    Changed main font size and text, added bottom image

--- client/pages/index.js
+++ client/pages/index.js
@@ -4,36 +4,35 @@ import styles from "../styles/Home.module.css";
 import * as circle_text from "../public/icons/smallbubleArtboard_1.svg";
 
 export default function Home() {
-	return (
-		<div className={styles.holder}>
-			<div className={styles.navigation}>
-				<div className={styles.hamburger}></div>
-			</div>
-			<div className={styles.first}>
-				<div className={styles.left_side}>
-					<div>Hi There</div>
-					<div className={styles.cuty_picture}></div>
-				</div>
-				<div className={styles.right_side}>
-					<div className={styles.right_side_text}>
-						<div>
-							I am a digital communicator, currently honing my craft at the
-							University of Louisville.
-						</div>
-						<div className={styles.coordinates}>38.2123° N, 85.7585° W</div>
-					</div>
-					<div className={styles.right_side_bar}>
-						<div className={styles.circle_text}></div>
-						{/* <div className={styles.circle_text}></div> */}
-					</div>
-				</div>
-			</div>
-			<div className={styles.second}>
-				<div className={styles.second_text}>
-					I am drawn towards modern design that clearly communicates with the
-					viewer. Take a look at some of my work below.
-				</div>
-			</div>
-		</div>
-	);
+  return (
+    <div className={styles.holder}>
+      <div className={styles.navigation}>
+        <div className={styles.hamburger}></div>
+      </div>
+      <div className={styles.first}>
+        <div className={styles.left_side}>
+          <div className={styles.title_text}>Hi There</div>
+          <div className={styles.cuty_picture}></div>
+        </div>
+        <div className={styles.right_side}>
+          <div className={styles.right_side_text}>
+            <div>
+              I am a digital communicator, currently honing my craft at the
+              University of Louisville.
+            </div>
+            <div className={styles.coordinates}>38.2123° N, 85.7585° W</div>
+          </div>
+          <div className={styles.right_side_bar}>
+            <div className={styles.circle_text}></div>
+          </div>
+        </div>
+      </div>
+      <div className={styles.second}>
+        <div className={styles.second_text}>
+          I am drawn towards modern design that clearly communicates with the
+          viewer. Take a look at some of my work below.
+        </div>
+      </div>
+    </div>
+  );
 }
