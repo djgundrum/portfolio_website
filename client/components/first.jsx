d9bccc026be532d7c277d48753f26ae3b9f5eb58
djgundrum <44683554+djgundrum@users.noreply.github.com>
2022-01-22 01:23:48 -0500

    compartmentalize home screen

---
+++ client/components/first.jsx
@@ -0,0 +1,47 @@
+import styles from "../styles/first.module.css";
+
+const First = () => {
+	return (
+		<html>
+			<head>
+				<link href="https://use.typekit.net/nht7ybu.css" rel="stylesheet" />
+			</head>
+			<body>
+				<div className={styles.holder}>
+					<div className={styles.navigation}>
+						<div className={styles.hamburger}></div>
+					</div>
+					<div className={styles.first}>
+						<div className={styles.left_side}>
+							<div className={styles.title_text}>Hi There</div>
+							<div className={styles.cuty_picture}></div>
+						</div>
+						<div className={styles.right_side}>
+							<div className={styles.right_side_text}>
+								<div>
+									I am a digital communicator <br></br> currently honing my
+									craft at <br></br>the University of Louisville.
+								</div>
+								<div className={styles.coordinates}>38.2123° N, 85.7585° W</div>
+							</div>
+							<div className={styles.right_side_bar}>
+								<div className={styles.circle_text}></div>
+							</div>
+						</div>
+					</div>
+					<div className={styles.second}>
+						<div className={styles.down_arrow}></div>
+						<div className={styles.second_image}></div>
+						<div className={styles.second_text}>
+							I am drawn towards modern design that <br></br> clearly
+							communicates with the viewer. <br></br>Take a look at some of my
+							work below.
+						</div>
+					</div>
+				</div>
+			</body>
+		</html>
+	);
+};
+
+export { First };
--- client/pages/index.js
+++ client/pages/index.js
@@ -1,47 +1,5 @@
-import Head from "next/head";
-import Image from "next/image";
-import styles from "../styles/Home.module.css";
+import { First } from "../components/first";
 
 export default function Home() {
-  return (
-    <html>
-      <head>
-        <link href="https://use.typekit.net/nht7ybu.css" rel="stylesheet" />
-      </head>
-      <body>
-        <div className={styles.holder}>
-          <div className={styles.navigation}>
-            <div className={styles.hamburger}></div>
-          </div>
-          <div className={styles.first}>
-            <div className={styles.left_side}>
-              <div className={styles.title_text}>Hi There</div>
-              <div className={styles.cuty_picture}></div>
-            </div>
-            <div className={styles.right_side}>
-              <div className={styles.right_side_text}>
-                <div>
-                  I am a digital communicator <br></br> currently honing my
-                  craft at <br></br>the University of Louisville.
-                </div>
-                <div className={styles.coordinates}>38.2123° N, 85.7585° W</div>
-              </div>
-              <div className={styles.right_side_bar}>
-                <div className={styles.circle_text}></div>
-              </div>
-            </div>
-          </div>
-          <div className={styles.second}>
-            <div className={styles.down_arrow}></div>
-            <div className={styles.second_image}></div>
-            <div className={styles.second_text}>
-              I am drawn towards modern design that <br></br> clearly
-              communicates with the viewer. <br></br>Take a look at some of my
-              work below.
-            </div>
-          </div>
-        </div>
-      </body>
-    </html>
-  );
+	return <First />;
 }
